ce17b763354c7505618d3c26a8e89bc352aa99e1
devBublik <ew.sorokina2011@yandex.ru>
2020-10-03 01:33:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (array != null && array != '') {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array != null && array != '') {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length !== 0) {
+        let result = array.reduce((sum, current) => sum + current, 0);
+        return result / array.length;
+    } else {
+        return 0;
+    }
+};
